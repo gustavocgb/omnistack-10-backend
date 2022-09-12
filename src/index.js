a50d6e84a83b7fd57fb4c8914bc470cbbb8fe499
Gustavo Barbosa <60071133+gustavocgb@users.noreply.github.com>
2022-09-12 14:18:02 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const server = http.Server(app);
 
 setupWebsocket(server);
 
-mongoose.connect('mongodb+srv://gustavocgb:451027@cluster0.uwng3.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect(, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
